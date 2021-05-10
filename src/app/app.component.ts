5d07c8785c6768ce84b5330aea26f66f1b1bbc74
dat.ngthanh1705 <dat.ngthanh1705@gmail.com>
2021-05-10 16:55:49 +0700

    add ant design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'fpt-university-medical-room-management-website';
+  isCollapsed = false;
 }
