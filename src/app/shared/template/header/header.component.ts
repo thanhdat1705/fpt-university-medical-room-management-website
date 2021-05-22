96407289145e6f995ee09969d087ac8280eefc7f
TanNHN <nhattan9101999@gmail.com>
2021-05-23 04:21:33 +0700

    update responsive, hide effect for SideNav

--- src/app/shared/template/header/header.component.ts
+++ src/app/shared/template/header/header.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { SideNavComponent } from '../side-nav/side-nav.component';
+import { SideNavService } from '../side-nav/side-nav.service';
+
+
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
+
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+	toggleActive:boolean = false;
+
+  constructor(private sidenav: SideNavService) {
+  }
 
   ngOnInit(): void {
   }
+
+  ToggleNav() {
+
+    console.log('Clicked');
+    this.toggleActive = !this.toggleActive;
+		this.sidenav.toggle();
+	}
 
 }
--- src/app/shared/template/side-nav/side-nav.component.ts
+++ src/app/shared/template/side-nav/side-nav.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { DeviceDetectorService } from 'ngx-device-detector';
+import { SideNavService } from './side-nav.service';
 
 @Component({
   selector: 'app-side-nav',
@@ -6,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./side-nav.component.scss']
 })
 export class SideNavComponent implements OnInit {
+  @ViewChild('drawer', {static: true}) sidenav: MatSidenav;
+  deviceInfo = null;
+  isMobile = null;
+  ToggleMode = "side";
 
-  constructor() { }
+  constructor(private sidenavService: SideNavService, private deviceService: DeviceDetectorService) {
+    this.selectMode();
+
+  }
+
+
+  selectMode() {
+    this.deviceInfo = this.deviceService.getDeviceInfo();
+    this.isMobile = this.deviceService.isMobile();
+    console.log(this.isMobile);
+    if(this.isMobile){
+      this.ToggleMode = "over";
+    }else{
+      this.ToggleMode = "side";
+    }
+  }
 
   ngOnInit(): void {
+		this.sidenavService.setSidenav(this.sidenav);
+    
+  }
+  
+  Toggle(){
+    this.sidenav.toggle();
   }
 
 }
