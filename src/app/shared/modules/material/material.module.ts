887007fcc53fd0e7c7377d446fdedc047bb343a5
TanNHN <nhattan9101999@gmail.com>
2021-05-25 02:32:28 +0700

    flex content

--- src/app/shared/modules/material/material.module.ts
+++ src/app/shared/modules/material/material.module.ts
@@ -4,12 +4,20 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatExpansionModule} from '@angular/material/expansion';
+import {MatTableModule} from '@angular/material/table';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatPaginatorModule} from '@angular/material/paginator';
+
+
 
 const MATERIAL_MODULES = [
   MatToolbarModule,
   MatIconModule,
   MatSidenavModule,
-  MatExpansionModule
+  MatExpansionModule,
+  MatTableModule,
+  MatFormFieldModule,
+  MatPaginatorModule
 ];
 
 @NgModule({
