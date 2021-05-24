e78408ae7994cb1babc509201749c4ba1350eb9c
TanNHN <nhattan9101999@gmail.com>
2021-05-24 16:51:00 +0700

    template

--- src/app/layouts/common-layout/common-layout.component.ts
+++ src/app/layouts/common-layout/common-layout.component.ts
@@ -19,61 +19,36 @@ export class CommonLayoutComponent  {
     isExpand: boolean;
     selectedHeaderColor: string;
 
-    constructor(private router: Router,  private activatedRoute: ActivatedRoute, private themeService: ThemeConstantService) {
-        this.router.events.pipe(
-            filter(event => event instanceof NavigationEnd),
-            map(() => {
-                let child = this.activatedRoute.firstChild;
-                while (child) {
-                    if (child.firstChild) {
-                        child = child.firstChild;
-                    } else if (child.snapshot.data && child.snapshot.data['headerDisplay']) {
-                        return child.snapshot.data['headerDisplay'];
-                    } else {
-                        return null;
-                    }
-                }
-                return null;
-            })
-        ).subscribe( (data: any) => {
-            this.contentHeaderDisplay = data;
-        });
+    constructor(private router: Router) {
+       
     }
 
     ngOnInit() {
-        this.breadcrumbs$ = this.router.events.pipe(
-            startWith(new NavigationEnd(0, '/', '/')),
-            filter(event => event instanceof NavigationEnd),distinctUntilChanged(),
-            map(data => this.buildBreadCrumb(this.activatedRoute.root))
-        );
-        this.themeService.isMenuFoldedChanges.subscribe(isFolded => this.isFolded = isFolded);
-        this.themeService.isSideNavDarkChanges.subscribe(isDark => this.isSideNavDark = isDark);
-        this.themeService.selectedHeaderColor.subscribe(color => this.selectedHeaderColor = color);   
-        this.themeService.isExpandChanges.subscribe(isExpand => this.isExpand = isExpand);     
+       
     }
 
-    private buildBreadCrumb(route: ActivatedRoute, url: string = '', breadcrumbs: IBreadcrumb[] = []): IBreadcrumb[] {
-        let label = '', path = '/', display = null;
+    // private buildBreadCrumb(route: ActivatedRoute, url: string = '', breadcrumbs: IBreadcrumb[] = []): IBreadcrumb[] {
+    //     let label = '', path = '/', display = null;
 
-        if (route.routeConfig) {
-            if (route.routeConfig.data) {
-                label = route.routeConfig.data['title'];
-                path += route.routeConfig.path;
-            }
-        } else {
-            label = 'Dashboard';
-            path += 'dashboard';
-        }
+    //     if (route.routeConfig) {
+    //         if (route.routeConfig.data) {
+    //             label = route.routeConfig.data['title'];
+    //             path += route.routeConfig.path;
+    //         }
+    //     } else {
+    //         label = 'Dashboard';
+    //         path += 'dashboard';
+    //     }
 
-        const nextUrl = path && path !== '/dashboard' ? `${url}${path}` : url;
-        const breadcrumb = <IBreadcrumb>{
-            label: label, url: nextUrl
-        };
+    //     const nextUrl = path && path !== '/dashboard' ? `${url}${path}` : url;
+    //     const breadcrumb = <IBreadcrumb>{
+    //         label: label, url: nextUrl
+    //     };
 
-        const newBreadcrumbs = label ? [...breadcrumbs, breadcrumb] : [...breadcrumbs];
-        if (route.firstChild) {
-            return this.buildBreadCrumb(route.firstChild, nextUrl, newBreadcrumbs);
-        }
-        return newBreadcrumbs;
-    }
+    //     const newBreadcrumbs = label ? [...breadcrumbs, breadcrumb] : [...breadcrumbs];
+    //     if (route.firstChild) {
+    //         return this.buildBreadCrumb(route.firstChild, nextUrl, newBreadcrumbs);
+    //     }
+    //     return newBreadcrumbs;
+    // }
 }
--- src/app/shared/modules/material/material.module.ts
+++ src/app/shared/modules/material/material.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 const MATERIAL_MODULES = [
   MatToolbarModule,
   MatIconModule,
-  MatSidenavModule
+  MatSidenavModule,
+  MatExpansionModule
 ];
 
 @NgModule({
